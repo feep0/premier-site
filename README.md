a8418624ea89c84c1081aab5d230a73721c2b038
feep0 <116185869+feep0@users.noreply.github.com>
2023-01-18 16:51:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# premier-site
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/zahara-nour-club-info-jprd7q)
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,24 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>A propos de cette app</h1>
+
+	<p>
+		Cette app est un premier petit projet du club informatique du Lycée Voltaire pour apprendre les
+		bases de développement Web à l'aide de technologies modernes.
+	</p>
+	<p>
+		Le code source est sauvegardée sur <a href="https://github.com">Github</a>.
+	</p>
+	<p>
+		L'environnement de développement est le browser Google Chrome à l'aide du site
+		<a href="https://stackblitz.com">Stackblitz</a>.
+	</p>
+	<p>
+		Pour nous aider à programmer l'app, nous utilisons le metaframework
+		<a href="https://kit.svelte.dev">SvelteKit</a>.
+	</p>
+</div>
